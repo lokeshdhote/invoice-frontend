98763ab0bfaa666290420fa7a638d24c10788b46
lokeshdhote <lokeshdhote2004@gmail.com>
2025-02-02 23:25:17 +0530

    url updates

--- src/utlis/Axios.jsx
+++ src/utlis/Axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
 
-    baseURL: "http://localhost:3000",    
+    // baseURL: "http://localhost:3000", 
+    baseURL:"https://dashboard.render.com/web/srv-cufr1uqj1k6c73ftgs20/deploys/dep-cufr1v2j1k6c73ftgs80",   
    
     withCredentials: true,
   });
